1841f8d08e0372a81afce48c725ea17fd384dab5
jefferson escocard marins <jescocard@gmail.com>
2015-09-07 14:18:39 -0300

    Primeira Alteração

---
+++ VaiFundos/VaiFundos/Aplicacao.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaiFundos
+{
+    class Aplicacao
+    {
+        public float valorAplicacao;
+        private DateTime dataAplicacao;
+        private Cliente cliente;
+
+        public Aplicacao(float valor, Cliente clt)
+        {
+            this.valorAplicacao = valor;
+            this.cliente = clt;
+            this.dataAplicacao = DateTime.Now;
+        }
+        
+    }
+   
+}
---
+++ VaiFundos/VaiFundos/FundoDeInvestimento.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaiFundos
+{
+    class FundoDeInvestimento
+    {
+        protected string nome;
+        protected string cod;
+        protected List<Aplicacao> aplicacoes;
+        
+
+        public void resgate()
+        {
+
+
+        }
+        public void Aplicar(Aplicacao nova,int cod)
+        {
+            this.aplicacoes.Add(nova);
+        }
+    }
+    
+
+
+}
